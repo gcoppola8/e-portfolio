047653c60604ac5badc4bda05def7a5142e0bd12
Gennaro Coppola <coppola612@gmail.com>
2025-09-05 13:46:19 +0200

    disable the background

--- js/backgroundScene.js
+++ js/backgroundScene.js
@@ -21,44 +21,44 @@ renderer.domElement.style.zIndex = '-1';
 renderer.domElement.style.pointerEvents = 'none';
 document.body.insertBefore(renderer.domElement, document.body.firstChild);
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.autoRotate = true;
+// // Controls
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.autoRotate = true;
 
-// Lighting
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(2, 2, 5);
-scene.add(light);
-scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+// // Lighting
+// const light = new THREE.DirectionalLight(0xffffff, 1);
+// light.position.set(2, 2, 5);
+// scene.add(light);
+// scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
-// Load GLTF
-const loader = new GLTFLoader();
-let mixer, model;
+// // Load GLTF
+// const loader = new GLTFLoader();
+// let mixer, model;
 
-let shipStartY = 0;
-loader.load("./js/vendor/ship/scene.gltf", (gltf) => {
-    model = gltf.scene;
-    scene.add(model);
+// let shipStartY = 0;
+// loader.load("./js/vendor/ship/scene.gltf", (gltf) => {
+//     model = gltf.scene;
+//     scene.add(model);
 
-    // Set model to a random position off the left side of the screen
-    shipStartY = (Math.random() - 0.5) * 30; // Range [-15, 15]
-    model.position.set(-8, shipStartY, 0);
-    model.scale.set(1.5, 1.5, 1.5);
-});
+//     // Set model to a random position off the left side of the screen
+//     shipStartY = (Math.random() - 0.5) * 30; // Range [-15, 15]
+//     model.position.set(-8, shipStartY, 0);
+//     model.scale.set(1.5, 1.5, 1.5);
+// });
 
-camera.position.set(11, 15, 15);
-controls.update();
+// camera.position.set(11, 15, 15);
+// controls.update();
 
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
-function animate() {
-    requestAnimationFrame(animate);
+// function animate() {
+//     requestAnimationFrame(animate);
 
-    const delta = clock.getDelta();
-    controls.update(delta);
-    renderer.render(scene, camera);
-}
-animate();
+//     const delta = clock.getDelta();
+//     controls.update(delta);
+//     renderer.render(scene, camera);
+// }
+// animate();
 
 window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
